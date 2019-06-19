337043309b6f62348d79e0f9fbc5d9c698cbfa0f
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-19 13:58:22 -0400

    wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,7 +19,13 @@ export class HomeComponent implements AfterViewInit, OnInit {
   title: string;
 
   poetry: MatTableDataSource<Poem> = new MatTableDataSource();
-  poetryColumns: string[] = ['position', 'author', 'title', 'linecount', 'button'];
+  poetryColumns: string[] = [
+    'position',
+    'author',
+    'title',
+    'linecount',
+    'button',
+  ];
 
   selectedPoem: Poem;
 
@@ -38,67 +44,22 @@ export class HomeComponent implements AfterViewInit, OnInit {
   }
 
   getPoems() {
-    this.http.get('assets/poems.json').subscribe((res: any) => {
-      console.log(res);
+    this.http.get('assets/poems.json').subscribe((res: Poem[]) => {
+      console.log(res[7]);
     });
+  }
+
+  getWords(poem: Poem) {
+    const words = [];
+
+    for (const word of poem.lines.join().split(' ')) {
+      console.log(word);
+    }
   }
 
   filterTable(filterValue: string) {
     this.poetry.filter = filterValue.trim().toLowerCase();
   }
-
-  // searchPoetry() {
-  //   if (this.poemQuery.text === '') {
-  //     return;
-  //   }
-
-  //   this.data.getPoetry(this.poemQuery).subscribe((res: any) => {
-  //     if (res.status) {
-  //       // error goes here
-  //       return;
-  //     }
-
-  //     if (res.length < 1) {
-  //       // error goes here
-  //       return;
-  //     } else {
-  //       console.log(res.length);
-
-  //       const poems = [];
-  //       this.poetry.data = [];
-
-  //       for (let i = 0; i < res.length; i++) {
-  //         const poem: Poem = res[i];
-
-  //         poems.push({
-  //           index: i + 1,
-  //           ...poem,
-  //         });
-  //       }
-
-  //       // localStorage.setItem('poems', JSON.stringify(poems));
-
-  //       this.poetry.data = poems;
-  //     }
-  //   });
-  // }
-
-  // search_box_placeholder(): string {
-  //   const searchText = 'Search by';
-
-  //   switch (this.poemQuery.inputField) {
-  //     case 'author':
-  //       return `${searchText} Author`;
-  //     case 'title':
-  //       return `${searchText} Title`;
-  //     case 'lines':
-  //       return `${searchText} Phrase(s)`;
-  //     case 'linecount':
-  //       return `${searchText} Line Count`;
-  //     default:
-  //       break;
-  //   }
-  // }
 
   selectPoem(poem: Poem) {
     console.log(poem);
